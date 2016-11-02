64e67d487810ea790266660fa972eb8d318640c0
vagrant <vagrant@vagrant.vm>
2016-11-02 16:08:27 +0000

    Fixed adding colors to bins. TODO: Tally most common bins and select most common color from those bins.

--- image_manipulation.py
+++ image_manipulation.py
@@ -9,17 +9,14 @@ import math
 # Ex. round_down(156) --> 150
 # Ex. round_down(156, 5) --> 155
 def round_down(num, divisor=10):
+    """ Round down to nearest divisor (default 10) """
     return num - (num % divisor)
-
-
-def get_bucket_index(hue):
-    return int(math.floor(hue/10))
-
 
 
 ################### Image Analysis ###################
 
 def get_image_object(file_path):
+    """ Creates Image object with HSV color profile """
 
     # Prepend os file path to image 
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -34,9 +31,10 @@ image = get_image_object('img/sunset.jpg')
 
 
 # default to 100 most common colors
-def get_top_colors_raw(image, num_colors=1000):
+def get_common_colors(image, num_colors=1000):
+    """ """
     
-    # Get width and height for .getcolors()
+    # Width and height for .getcolors()
     w, h = image.size
 
     # Returns a list of unsorted tuples
@@ -47,16 +45,14 @@ def get_top_colors_raw(image, num_colors=1000):
     get_colors.sort(reverse=True)
 
     # Return most common colors
-    top_colors = get_colors[:num_colors]
+    common_colors = get_colors[:num_colors]
 
-    return top_colors # List of raw data tuples. Length of list is num_colors.
+    return common_colors # List of raw data tuples. Length of list is num_colors.
 
 
-top_colors = get_top_colors_raw(image)
+common_colors = get_common_colors(image) # add second argument for how many colors
 
 print '#############################'
-# print 'top_colors: ', len(top_colors), top_colors
-
 
 # Get raw data
 
@@ -71,6 +67,7 @@ print '#############################'
 
 
 def create_color_bins():
+    """ """
     color_bins = []
 
     for _ in xrange(36):
@@ -81,32 +78,34 @@ def create_color_bins():
 
 color_bins = create_color_bins()
 
-# This will become top_colors with real data
-image_tuples = [(32212, (0, 0, 9)), (28376, (0, 0, 10)), (19168, (155, 47, 16)), (18000, (155, 51, 15)), (15429, (155, 42, 18)), (14265, (0, 0, 14)), (12679, (0, 0, 11)), (12460, (155, 45, 17)), (12452, (155, 40, 19)), (11642, (0, 0, 8)), (11571, (0, 0, 13)), (10700, (155, 54, 14)), (10261, (154, 78, 26)), (9488, (154, 75, 27)), (9328, (0, 0, 12)), (9030, (155, 69, 11)), (8945, (155, 38, 20)), (8812, (154, 81, 25)), (8606, (0, 0, 16)), (8034, (155, 63, 12)), (7986, (154, 72, 28)), (7855, (155, 58, 13)), (7756, (155, 36, 21)), (7406, (155, 34, 22)), (7313, (154, 65, 31)), (7136, (154, 85, 24)), (7066, (0, 0, 15)), (6899, (154, 68, 30)), (6827, (154, 70, 29)), (6777, (106, 34, 15))]
+# Remove this once there is real data
+top_colors = [(32212, (0, 0, 9)), (28376, (35, 0, 10)), (19168, (23, 47, 16)), (18000, (27, 51, 15)), (15429, (230, 42, 18)), (14265, (120, 0, 14)), (12679, (101, 0, 11)), (12460, (79, 45, 17)), (12452, (155, 40, 19)), (11642, (0, 0, 8)), (11571, (0, 0, 13)), (10700, (155, 54, 14)), (10261, (154, 78, 26)), (9488, (154, 75, 27)), (9328, (0, 0, 12)), (9030, (60, 69, 11)), (8945, (155, 38, 20)), (8812, (165, 81, 25)), (8606, (0, 0, 16)), (8034, (155, 63, 12)), (7986, (154, 72, 28)), (7855, (155, 58, 13)), (7756, (155, 36, 21)), (7406, (155, 34, 22)), (7313, (154, 65, 31)), (7136, (154, 85, 24)), (7066, (0, 0, 15)), (6899, (154, 68, 30)), (6827, (154, 70, 29)), (6777, (106, 34, 15))]
 
 
 
 def fill_color_bins(color_bins, image_tuples):
+    """ """
     
     for raw_tuple in image_tuples:
-        print 'raw_tuple', raw_tuple
+        # Round hue down to the nearest 10
         adjusted_hue = round_down(raw_tuple[1][0])
-        print 'adjusted_hue', adjusted_hue
 
-##############################################
-# THIS IS WEIRD
-        idx = get_bucket_index(adjusted_hue)
-        print "idx", idx
+        # Find the bucket: 
+        bucket_idx = int(math.floor(adjusted_hue/10))
 
-        color_bins.append(raw_tuple)
+        color_bins[bucket_idx].append(raw_tuple)
 
+    print 'color_bins after adding', len(color_bins), color_bins
     return color_bins
 
 
 
-answer = fill_color_bins(color_bins, image_tuples)
+answer = fill_color_bins(color_bins, top_colors)
 
-print answer
+
+def tally_color_bins(color_bins):
+    """ """
+    pass
 
 # [
 #     # Implied index based on hue
