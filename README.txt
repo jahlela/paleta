31f93f505ce0fa3771b4d89b42da22eaf0c95eb5
vagrant <vagrant@vagrant.vm>
2016-11-02 00:23:13 +0000

    Removed some unused resources in README and changed default color type to HSV in image_manipulaiton.py

--- README.txt
+++ README.txt
@@ -8,11 +8,4 @@ Things I might need:
 
 colorsys (python)
 
-
-http://stackoverflow.com/questions/10014271/generate-random-color-distinguishable-to-humans
-
-Google Cloud SDK 
-    >> curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
-    >> ./google-cloud-sdk/install.sh
-
-    [/home/vagrant/.bashrc]
+https://www.sitepoint.com/manipulating-images-with-the-python-imaging-library/
--- image_manipulation.py
+++ image_manipulation.py
@@ -5,13 +5,15 @@ import os.path
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os_file_path = os.path.join(script_dir, 'img/jellyfish_color.jpg')
 
-im = Image.open(os_file_path)
+im = Image.open(os_file_path).convert('HSV')
 w, h = im.size
+
+
 
 
 print '#############################'
 get_colors = im.getcolors(w * h)
 get_colors.sort(reverse=True)
-top_colors = get_colors[:10]
+top_colors = get_colors[:50]
 
 print 'top_colors', top_colors
