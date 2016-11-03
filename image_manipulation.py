45028a1677030da8d765fb884f38a0206c8fbaa6
vagrant <vagrant@vagrant.vm>
2016-11-03 19:50:23 +0000

    Added a red test image of uniform color. Finished outputing top hex colors.

--- image_manipulation.py
+++ image_manipulation.py
@@ -6,12 +6,15 @@ import os.path
 import math
 import colorsys
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
 
 ################### HELPER FUNCTIONS ###################
 
 # Takes two integers, num and divisor. Rounds num down to nearest divisor (default 10)
 # Ex. round_down(156) --> 150
-# Ex. round_down(156, 5) --> 155
 def round_down(num, divisor=10):
     """ Round down to nearest divisor (default 10) """
     return num - (num % divisor)
@@ -32,7 +35,7 @@ def get_image_object(file_path):
     return image
 
 # Set test image
-image = get_image_object('img/sunset.jpg')
+image = get_image_object('img/octopus.png')
 
 def get_common_colors(image, num_colors=1000):
     """ Returns a list of tuples with most common colors and their pixel count. 
@@ -61,6 +64,20 @@ def get_common_colors(image, num_colors=1000):
 
 common_colors = get_common_colors(image) # add second argument for how many colors
 
+print 'common_colors from real image, limit 100', common_colors[-100:]
+
+# common_colors = [
+#               (32212, (312, 0, 9)), (28376, (35, 0, 10)), (19168, (23, 47, 16)), 
+#               (18000, (27, 51, 15)), (15429, (230, 42, 18)), (14265, (120, 0, 14)), 
+#               (12679, (101, 0, 11)), (12460, (79, 45, 17)), (12452, (155, 40, 19)), 
+#               (11642, (9, 0, 8)), (11571, (2, 0, 13)), (10700, (155, 54, 14)), 
+#               (10261, (154, 78, 26)), (9488, (154, 75, 27)), (9328, (0, 0, 12)), 
+#               (9030, (60, 69, 11)), (8945, (155, 38, 20)), (8812, (165, 81, 25)), 
+#               (8606, (0, 0, 16)), (8034, (155, 63, 12)), (7986, (154, 72, 28)), 
+#               (7855, (155, 58, 13)), (7756, (155, 36, 21)), (7406, (155, 34, 22)), 
+#               (7313, (154, 65, 31)), (7136, (154, 85, 24)), (7066, (7, 0, 15)), 
+#               (6899, (154, 68, 30)), (6827, (154, 70, 29)), (6777, (106, 34, 15))
+#               ]
 
 def create_color_bins():
     """ Create an empty list with 36 empty lists that will represent the 36 possible
@@ -81,6 +98,8 @@ color_bins = create_color_bins()
 def fill_color_bins(color_bins, image_tuples):
     """ Takes a list of 36 empty lists and returns a list where each empty 
         list (bin) is now filled with tuples for colors belonging to that bin. """
+
+    logging.debug('Length of color_bins should be 36 and is {:d}'.format(len(color_bins)))
 
     for raw_tuple in image_tuples:
 
@@ -132,24 +151,49 @@ def tally_color_bins(color_bins):
 
 
 top_colors = tally_color_bins(color_bins)
-print 'top_colors', top_colors
+
+def get_display_colors(top_colors, color_limit):
+    """ Takes a list of tuples of format (count, (Hue, Saturation, Value)) 
+        and returns a list of hex colors. Length is color_limit. """
+
+    print '\n top_colors', top_colors
+
+    limited_colors = top_colors[:color_limit]
+    display_colors = []
+
+    # Convert each color from HSV --> RGB --> hex because strings are better
+    for color_tuple in limited_colors:
+
+        # Unpack HSV values from tuple
+        hue, saturation, value = color_tuple[1]
+
+        # Use colorsys to convert HSV --> RGB 
+        # Must divide by a float, or colorsys will return (0,0,0)
+        rgb_color = colorsys.hsv_to_rgb(hue/360.0, saturation/255.0, value/255.0)
+
+        # Colour returns black and white as 'black' and 'white, not as a hex
+        if rgb_color == (0,0,0):
+            hex_color = '#000'
+        elif rgb_color == (1,1,1):
+            hex_color = '#FFF'
+        else:
+            # unpack to individual values for hex transformation
+            red, green, blue = rgb_color
+            # convert from RBG --> hex (default for Colour object)
+            hex_color = Color(rgb=(red, green, blue)).get_hex()
+    
+        print 'hex_color', hex_color
+
+        display_colors.append(hex_color)
+            
+
+    return display_colors
 
 
-def get_colors_for_display(top_colors, color_count_limit, color_type):
-    """  """
+display_colors = get_display_colors(top_colors, 5)
+print '\n display_colors', display_colors
 
-    display_colors = top_colors[:color_count_limit-1]
 
-    # I know, sucks to use two libraries for color conversions. 
-    for color in display_colors:
-        if color_type == 'rgb':
-            # Use colorsys to convert HSV --> RGB
-            color = colorsys.hsv_to_rgb(color)
-        elif color_type  == 'hex':
-            # Create Colour library object
-            color = Color(color)
-            # Use Colour to convert RGB --> hex
-            color.hex
 
 
 
