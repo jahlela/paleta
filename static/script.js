47f2a8637dab1a7975541a9b7d8a44421db0160f
jahlela <jahlela5@gmail.com>
2016-11-21 17:22:14 -0800

    Fixed naming typos in AJAX, removed register.html.

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
-
 $(function() {
     function addUserImageToDB() {
         var imageId = $(this).data("image");
+
         payload = {"image_id": imageId};
 
         $.post("/favorite_image", payload, function(data) {
@@ -11,32 +11,41 @@ $(function() {
             star.addClass("star-full glyphicon-star");
             star.removeClass("star-empty glyphicon-star-empty");
 
-            star.on("click", removeImage);
+            // star.off("click", ".favorite-image-btn");
+            star.on("click", removeUserImageFromDB);
         });
     }
 
 
     function removeUserImageFromDB() {
         var imageId = $(this).data("image");
+
         payload = {"image_id": imageId};
 
         $.post("/remove_image", payload, function(data) {
+            console.log(imageId);
             var star = $("#image-" + imageId);
 
             star.addClass("star-empty glyphicon-star-empty");
             star.removeClass("star-full glyphicon-star");
 
-            star.on("click", favoriteImage);
+            star.on("click", addUserImageToDB);
         });
     }
 
-    function removeImageFromPage() {
+    function removeImageFromProfile() {
+        console.log("Removing closest .image-with-palette")
         $(this).closest('.image-with-palette').remove();
 
     }
 
-    $(".profile-image").on("click", removeImageFromPage);
+
+
+    $(".profile-image").on("click", removeImageFromProfile);
     $(".star-empty").on("click", addUserImageToDB);
     $(".star-full").on("click", removeUserImageFromDB);
+
+
+
 
 });
